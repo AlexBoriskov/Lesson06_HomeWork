ee6cf2b3c7814b87f7a25e38a3a2205eeb9fde41
Alex <sasha-boriskov@yandex.ru>
2023-03-29 22:44:58 +0300

    Поиск положительных чисел

---
+++ Task41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int Plus (int [] array)
+{
+    int res = 0;
+    for (int i=0; i<array.Length; i++)
+        if (array[i]>0)
+            res++;
+    return res;
+}
+
+Console.Clear();
+
+// Console.Write ("Введите числа: ");
+int [] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine (string.Join(", ", array));
+int res = Plus(array);
+Console.WriteLine (res);
